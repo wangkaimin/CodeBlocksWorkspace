eda11e0805d394562b082163443ab0ed1bc944d1
WangKaimin <info@wangkaimin.com>
2013-05-11 18:32:06 +0800

    update 2013_05_06/main.c

--- 2013_05_11/main.c
+++ 2013_05_11/main.c
@@ -2,14 +2,24 @@
 #include <stdlib.h>
 void testSizeof(int a[10])
 {
-    printf("sizeof(a)=%d",sizeof(a));
+    int b[10];
+    int c[10]={0};
+    printf("sizeof(a)=%d\n",sizeof(a));
+    printf("sizeof(b)=%d\n",sizeof(b));
+    printf("strlen(a)=%d\n",strlen(a));
+    printf("strlen(b)=%d\n",strlen(b));
+    printf("strlen(c)=%d\n",strlen(c));
 }
-
+// 中文注释 测试是否乱码？
 int main()
 {
     int b[10];
-    printf("Hello world!\n");
+    int c[10]={0};
+    printf("#main begin#\n");
     printf("sizeof(b)=%d\n",sizeof(b));
+    printf("strlen(b)=%d\n",strlen(b));
+    printf("strlen(c)=%d\n",strlen(c));
+    printf("\n#testSizeof begin#\n");
     testSizeof(b);
     return 0;
 }
