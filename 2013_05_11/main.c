54b4b997a4adf64757b851e6bc39ddb6bd2cc539
WangKaimin <info@wangkaimin.com>
2013-05-11 18:40:01 +0800

    给sizeof和strlen的测试加上注释

--- 2013_05_11/main.c
+++ 2013_05_11/main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-void testSizeof(int a[10])
+#include <string.h>
+void testSizeofAndStrlen(int a[10])
 {
     int b[10];
     int c[10]={0};
+    // 整型数组作为参数 sizeof是指针类型的大小
     printf("sizeof(a)=%d\n",sizeof(a));
+    // 一般的局部整型数组 sizeof是单个整型的整数倍
     printf("sizeof(b)=%d\n",sizeof(b));
+    // 未初始化的strlen为随机值
     printf("strlen(a)=%d\n",strlen(a));
     printf("strlen(b)=%d\n",strlen(b));
+    // 初始化后正常
     printf("strlen(c)=%d\n",strlen(c));
 }
 // 中文注释 测试是否乱码？
@@ -19,7 +24,7 @@ int main()
     printf("sizeof(b)=%d\n",sizeof(b));
     printf("strlen(b)=%d\n",strlen(b));
     printf("strlen(c)=%d\n",strlen(c));
-    printf("\n#testSizeof begin#\n");
-    testSizeof(b);
+    printf("\n#testSizeofAndStrlen begin#\n");
+    testSizeofAndStrlen(b);
     return 0;
 }
