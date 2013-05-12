8f1d61f75b1bc9e58d4977f2e2dd3d324fd3967e
WangKaimin <info@wangkaimin.com>
2013-05-12 17:10:43 +0800

    switch分支

---
+++ switch分支/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i = 4;
+
+    switch (i)
+    {
+        default:
+        ;
+        case 3:
+            i += 5;
+            if ( i == 8)
+            {
+                i++;
+                if (i == 9) break;
+                i *= 2;
+            }
+            i -= 4;
+            break;
+        case 8:
+            i += 5;
+            break;
+    }
+    printf("i = %d\n", i);
+    return 0;
+}
