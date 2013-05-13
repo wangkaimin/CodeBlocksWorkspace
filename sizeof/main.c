e6f1ccbdafe840b3a62c734b4d3de009e6f21516
WangKaimin <info@wangkaimin.com>
2013-05-13 19:54:31 +0800

    update sizeof

--- sizeof/main.c
+++ sizeof/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef union
+{
+    char str[9]; //9
+    int i; // 4
+    long long ll; //8
+}test1;
+typedef struct {
+    int a[7]; //28
+    test1 test; //10
+    char c[7]; //7
+} test2;
+
+void sizeoftest()
+{
+    printf("sizeof(long long)=%d\n",sizeof(long long));
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    printf("sizeof(test1)=%d\n",sizeof(test1));
+    printf("sizeof(test2)=%d\n",sizeof(test2));
+    printf("%d\n",sizeof(test1)+sizeof(test2));
+    sizeoftest();
     return 0;
 }
