8f4a29ecd463d77d54945fbc6071a28028870d19
WangKaimin <info@wangkaimin.com>
2013-05-14 19:29:13 +0800

    修改指针与数组

--- 指针与数组/main.c
+++ 指针与数组/main.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define ARRAY_OR_POINTER 0
 int main()
 {
+    //
     int a[10] = {9,8,7,6,5,4,3,2,1,0};
+    int *p=NULL;
     printf("a \t is 0x%08x\n",a);
     printf("&a \t is 0x%08x\n",&a);
     printf("a+1 \t is 0x%08x\n",a+1);
+
+    // &a+1 ÊÇÔö¼ÓÁË10*4¸ö×Ö½ÚµÄ³¤¶È
     printf("&a+1 \t is 0x%08x\n",&a+1);
     printf("a+3 \t is 0x%08x\n",a+3);
     printf("&a[3] \t is 0x%08x\n",&a[3]);
-    //ç¼–è¯‘ä¸èƒ½é€šè¿‡çš„
+    //±àÒë²»ÄÜÍ¨¹ıµÄ
     //printf("&(a+3) \t is 0x%08x\n",&(a+3));
     printf("&a[2] \t is 0x%08x\n",&a[2]);
     printf("(&a)[3] \t is 0x%08x\n",(&a)[3]);
     printf("(a[3] \t is 0x%08x\n",a[3]);
+
+    printf("Ö¸ÕëÓëÊı×éµÄÇø±ğ\n");
+    p=a;
+    printf("p \t is 0x%08x\n",p);
+    p = a+1;
+    printf("p=a+1 \t is 0x%08x\n",p);
+    ++p;
+    // ´ËÊ±p×Ô¼Ó10¸ö×Ö½ÚµÄ³¤¶È
+    printf("++p \t is 0x%08x\n",p);
+
+    p=&a+1;
+    printf("p \t is 0x%08x\n",p);
+    ++p;
+    // ´ËÊ±p×Ô¼Ó10¸ö×Ö½ÚµÄ³¤¶È
+    printf("++p \t is 0x%08x\n",p);
     return 0;
 }
