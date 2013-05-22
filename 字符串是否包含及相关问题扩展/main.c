6aefb86e667f39615e290ef267ff3a2273b5e974
WangKaimin <info@wangkaimin.com>
2013-05-22 17:27:20 +0800

    字符串包含的素数方法

--- 字符串是否包含及相关问题扩展/main.c
+++ 字符串是否包含及相关问题扩展/main.c
@@ -4,6 +4,30 @@
 #define MAXHASH 256
 typedef int (* cmpstr)(const char *lstr, const char *sstr);
 // 最笨的方法 时间复杂度O(n^2)
+// 遇到字符串时，尽量使用指针，这样简洁
+int cmp_str00(const char *lstr, const char *sstr)
+{
+    int flag = 0;
+    char *temp = lstr;
+    while(*sstr != '\0')
+    {
+        while(*temp != '\0')
+        {
+            if(*sstr == *temp++)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 0)
+            return 1;
+        flag = 0;
+        sstr++;
+        temp = lstr;
+    }
+    return 0;
+}
+// 最笨的方法 时间复杂度O(n^2)
 int cmp_str01(char *longstring, char * shortstring)
 {
     int len1 = strlen(longstring);
@@ -15,7 +39,10 @@ int cmp_str01(char *longstring, char * shortstring)
         flag = 1;
         for(j=0;j<len1;j++)
             if(longstring[j] == shortstring[i])
+            {
                 flag = 0;
+                break;
+            }
         if(flag == 1)
             return 1; // shortstring不完全包含在longstring中
     }
@@ -38,12 +65,34 @@ int cmp_str02(const char *lstr, const char *sstr)
     }
     return 0;
 }
-
+// 素数法
+// 只能是小写字母，没有考虑字母多次出现
+int cmp_str03(const char* lstr, const char* sstr)
+{
+    int primenum[26] = { 2, 3, 5, 7, 11,
+                        13, 17, 19, 23, 29,
+                        31, 37, 41, 43, 47,
+                        53, 59, 61, 67, 71,
+                        73, 79, 83, 89, 97,
+                        101};
+    long long int product = 1;
+    int i=0;
+    for(i=0;i<strlen(lstr);i++)
+    {
+        product = product*primenum[lstr[i]-'a'];
+    }
+    for(i=0;i<strlen(sstr);i++)
+    {
+        if(product % (primenum[sstr[i]-'a']) != 0)
+            return 1;
+    }
+    return 0;
+}
 int main()
 {
-    char lstr[] = "wangkaimin.)&(^@*%&^$)(&$(#&(@";
-    char sstr[] = "kaimin.(&&*";
-    cmpstr cmp_str = cmp_str02;
+    char lstr[] = "wangkaimin";
+    char sstr[] = "kaimin";
+    cmpstr cmp_str = cmp_str03;
     if((*cmp_str)(lstr,sstr) == 0)
         printf("Yes.\n");
     else
